58e33f8bf9e86de03270347e461ab5b49d776827
SheppeR <spokusdev@gmail.com>
2024-09-07 21:24:37 +0200

    Dependency injection for the client

--- GenX.Client/App.xaml.cs
+++ GenX.Client/App.xaml.cs
@@ -1,5 +1,47 @@
-﻿namespace GenX.Client;
+﻿using Microsoft.Extensions.Hosting;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Sylver.HandlerInvoker;
+using Wpf.Ui;
+
+namespace GenX.Client;
 
 public partial class App
 {
+	private static readonly IHost _host = Host.CreateDefaultBuilder()
+		.ConfigureAppConfiguration(config => { _ = config.AddJsonFile("appsettings.json", false, true); })
+		.ConfigureServices(
+			(context, services) =>
+			{
+				_ = services.AddSingleton(context.Configuration);
+				services.AddHandlers();
+
+				_ = services.AddSingleton<MainWindow>();
+
+				_ = services.AddSingleton<INavigationService, NavigationService>();
+				_ = services.AddSingleton<ISnackbarService, SnackbarService>();
+				_ = services.AddSingleton<IContentDialogService, ContentDialogService>();
+			}
+		).UseSerilog()
+		.Build();
+
+	protected override async void OnStartup(StartupEventArgs e)
+	{
+		await _host.StartAsync();
+
+		var _window = _host.Services.GetRequiredService<MainWindow>();
+		_window.Show();
+
+		base.OnStartup(e);
+	}
+
+	protected override async void OnExit(ExitEventArgs e)
+	{
+		await _host.StopAsync(TimeSpan.FromSeconds(5));
+		_host.Dispose();
+
+		base.OnExit(e);
+	}
 }
