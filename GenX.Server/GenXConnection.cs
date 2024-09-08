3c5e4c3ffbb324021f485190f7b7b06ab37fd74f
SheppeR <spokusdev@gmail.com>
2024-09-08 14:48:45 +0200

    Using custom version of Network libreary with internal visibility to GenX.Server

---
+++ GenX.Server/GenXConnection.cs
@@ -0,0 +1,9 @@
+﻿using GenX.Server.Database.Entities;
+using Network;
+
+namespace GenX.Server;
+
+public abstract class GenXConnection : Connection
+{
+	public DbUser? User { get; set; }
+}
--- GenX.Server/Handlers/LoginHandler.cs
+++ GenX.Server/Handlers/LoginHandler.cs
@@ -1,7 +1,6 @@
 ﻿using GenX.Network.Packets.Login;
 using GenX.Server.Database;
 using Microsoft.EntityFrameworkCore;
-using Network;
 using Sylver.HandlerInvoker.Attributes;
 
 namespace GenX.Server.Handlers;
@@ -9,36 +8,44 @@ namespace GenX.Server.Handlers;
 [Handler]
 public class LoginHandler
 {
-    private readonly IServerContext _context;
+	private readonly IServerContext _context;
 
-    public LoginHandler(IServerContext context)
-    {
-	    _context = context;
-    }
+	public LoginHandler(IServerContext context)
+	{
+		_context = context;
+	}
 
-    [HandlerAction(typeof(LoginRequest))]
-    public async void OnHandle(LoginRequest request, Connection client)
-    {
-        var user = await _context.Users.FirstOrDefaultAsync(u=> u.Username != null && u.Username.Equals(request.Username));
+	[HandlerAction(typeof(LoginRequest))]
+	public async void OnHandle(LoginRequest request, GenXConnection client)
+	{
+		var user = await _context.Users.FirstOrDefaultAsync(u => u.Username != null && u.Username.Equals(request.Username));
 
-        LoginResult loginResult;
+		LoginResult loginResult;
 
-        if (user == null)
-        {
-            loginResult = LoginResult.AccountNotFound;
-        }
-        else
-        {
-            if (user.Password != null && !user.Password.ToUpper().Equals(request.PasswordHash.ToUpper()))
-            {
-                loginResult = LoginResult.LoginFail;
-            }
-            else
-            {
-                loginResult = user.Banned ? LoginResult.AccountBanned : LoginResult.LoginSuccess;
-            }
-        }
+		if (user == null)
+		{
+			loginResult = LoginResult.AccountNotFound;
+		}
+		else
+		{
+			if (user.Password != null && !user.Password.ToUpper().Equals(request.PasswordHash.ToUpper()))
+			{
+				loginResult = LoginResult.LoginFail;
+			}
+			else
+			{
+				if (user.Banned)
+				{
+					loginResult = LoginResult.AccountBanned;
+				}
+				else
+				{
+					loginResult = LoginResult.LoginSuccess;
+					client.User = user;
+				}
+			}
+		}
 
-        client.Send(new LoginResponse(loginResult, request));
-    }
+		client.Send(new LoginResponse(loginResult, request));
+	}
 }
